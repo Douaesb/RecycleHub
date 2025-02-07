67f24fb70ee1de50b6a4b0425dd6490538b01a45
Douae sb <douae.sb411@gmail.com>
2025-02-07 11:44:10 +0100

    REC-14-display-waste-requests-city

--- src/app/features/collections-list/collections-list.component.ts
+++ src/app/features/collections-list/collections-list.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { WasteRequest } from '../../shared/models/wasteRequest.model';
+import { selectAllWasteRequests } from '../../store/wasteRequest/waste-request.selectors';
+import { User } from '../../shared/models/auth.model';
+import { AuthService } from '../../core/services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-collections-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './collections-list.component.html',
-  styleUrl: './collections-list.component.scss'
+  styleUrls: ['./collections-list.component.scss']
 })
-export class CollectionsListComponent {
+export class CollectionsListComponent implements OnInit {
+  collectorRequests$!: Observable<WasteRequest[]>;
+  currentUser: User | null;
 
+  constructor(private store: Store, private authService: AuthService) {
+    this.currentUser = this.authService.getCurrentUser();
+  }
+
+  ngOnInit(): void {
+    if (this.currentUser?.role === 'collecteur') {
+      const userCity = this.currentUser.address.city;
+      this.collectorRequests$ = this.store.select(selectAllWasteRequests).pipe(
+        map((requests) =>
+          requests.filter((req) => req.address.city === userCity)
+        )
+      );
+    }
+  }
 }
